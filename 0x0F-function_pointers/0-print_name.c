016da6f5d8c55527c80acbe28fc7973b9a0cc58c
root <ayshkajee@gmail.com>
2023-04-19 05:19:01 -0700

    Whats my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that passes a name to printer function
+ * @name: the name to pass to function
+ * @f: function pointer
+ * Return: pointer to the allocated memory.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL && name != NULL)
+		f(name);
+}
